254e8a370e98184f383ffa50c1881e72b0a85ae7
Kenzo Mendoza <kenzom954@gmail.com>
2016-12-03 21:15:21 -0800

    added ignore-styles module to help run mocha tests with css

--- src/components/app.js
+++ src/components/app.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import NavBar from './navbar';
 import VotesContainer from '../containers/votes-container';
 import Footer from './footer';
-// import '../stylesheets/style.scss';
+import '../stylesheets/style.scss';
 
 export default class App extends Component {
   render() {
