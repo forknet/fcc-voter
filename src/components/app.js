98ef8d64cae81a61befaedb4f065f11e71a180a9
Kenzo Mendoza <kenzom954@gmail.com>
2016-12-04 15:51:05 -0800

    navbar to be container

--- src/components/app.js
+++ src/components/app.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import NavBar from './navbar';
+import NavBar from '../containers/navbar';
 import VotesContainer from '../containers/votes-container';
 import Footer from './footer';
 import '../stylesheets/style.scss';
